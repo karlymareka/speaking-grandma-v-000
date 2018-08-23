20a84bc14e600ef68027257f994c36af0ed778a8
Karly Mareka <karly.mareka@gmail.com>
2018-08-23 15:45:17 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,5 +17,5 @@ def speak_to_grandma(string)
   elsif string == string.upcase
     return "NO, NOT SINCE 1938!"
   else 
-  return "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
 end 
